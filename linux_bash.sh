7993f4df4f0feb34bc9eb3772015c571341a129d
MD. Towhidul Islam (TUHIN) <tuhintowhidul9@gmail.com>
2020-09-23 09:34:03 +0600

    Create linux_bash.sh

---
+++ linux_bash.sh
@@ -0,0 +1,6 @@
+
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+PS1="\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$(parse_git_branch)\[\e[00m\]$ "
